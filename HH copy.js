32312e14583eca10abb0c3b170177ed9ab46c42f
jdarmada <jdarmada@gmail.com>
2023-10-14 10:58:05 -0700

    added more qs

--- HH copy.js
+++ HH copy.js
@@ -37,7 +37,7 @@ const mergeSort = array => {
 
 const newArr = [5,4, 6, 2, 10, 11, 1, 9];
 
-console.log(mergeSort(newArr))
+// console.log(mergeSort(newArr))
 
 
 
@@ -55,5 +55,70 @@ longestCommonPrefix(["dog","racecar","car"]) --> "" (There is no common prefix a
 */
 
 const longestCommonPrefix = (strs) => {
+  let commonStr = "";
+  //base case
+  if(!strs.length) return commonStr;
+
+  // recursive case
+  for(let i = 0; i < strs.length; i++) {
+    if(strs[i][0] === strs[0][i + 1] && strs[0][i] === strs[0][i + 2]){
+      commonStr.concat(strs[0][0])
+    }
+  }
+  
+
+  return commonStr;
   
 };
+
+longestCommonPrefix(["flower","flow","flight"])
+
+
+
+/*
+
+Given an array of integers, sort the array in-place and return the array.
+Do not create a new array in memory. Instead, modify the array as given. Do not
+use the array sort method built in to the array prototype while solving the
+problem. The integers should be in ascending order from left to right.
+
+We will sort the array using a strategy called selection sort, which works as
+follows. First, put the smallest number in the array at position 0. Then, put
+the second-smallest number in the array at position 1. Then, put the
+third-smallest number in the array at position 2 etc. After going through the
+whole array, the array will end up being sorted.
+
+*/
+
+const selectionSort = array => {
+  
+};
+
+/*
+
+Extension:
+
+Given an array of integers, sort the array in-place and return the array.
+Do not create a new array in memory. Instead, modify the array as given. Do not
+use the array sort method built in to the array prototype while solving the
+problem. The integers should be in ascending order from left to right.
+
+For this extension, we will use a strategy called insertion sort, which works as
+follows. Imagine that the first k - 1 numbers of the array are in ascending
+order. We take the kth number and insert it into the correct position amongst
+the k - 1 numbers such that now, the first k numbers of the array are in
+ascending order. In other words:
+
+The first value in the array is considered to be already fine.
+The second value is either placed before/after the first value.
+The third value is inserted in the correct position amongst the first two values.
+The fourth value is inserted in the correct position amongst the first three values.
+etc.
+
+*/
+
+const insertionSort = array => {
+  
+};
+
+
