ae1d6e1d8d79010d2a1354b74c3b325c3979a0cf
jdarmada <jdarmada@gmail.com>
2023-10-18 09:16:50 -0700

    new

--- HH copy.js
+++ HH copy.js
@@ -195,3 +195,38 @@ const substringNonrepeating = str => {
 };
 
 
+/*
+
+Given n pairs of parentheses, write a function to generate all combinations of
+well-formed parentheses.
+
+For example, given n = 2, a solution set is:
+
+[
+  "(())",
+  "()()"
+]
+
+Given n = 3, a solution set is:
+
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
+
+Given n = 0, a solution set is:
+
+[
+  ""
+]
+
+*/
+
+const generateParentheses = n => {
+  
+};
+
+
