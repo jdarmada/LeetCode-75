ca6f706015453aa95fe5675d1d922f31ac05d115
jdarmada <jdarmada@gmail.com>
2023-12-23 20:11:20 -0800

    insertion

--- HH-copy.js
+++ HH-copy.js
@@ -140,9 +140,26 @@ etc.
 
 */
 
-const insertionSort = array => {
-  
-};
+function insertionSort(arr) {
+  const n = arr.length;
+
+  for (let i = 1; i < n; i++) {
+    let current = arr[i];
+    let j = i - 1;
+
+    // Move elements of arr[0..i-1] that are greater than current
+    // to one position ahead of their current position
+    while (j >= 0 && arr[j] > current) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+
+    // Insert the current element in its correct position
+    arr[j + 1] = current;
+  }
+
+  return arr;
+}
 
 /* 
 Determine whether a target substring can be found within a string! 
